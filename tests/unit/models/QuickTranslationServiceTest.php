2ffa9de0c728ba2454c0701f3a585910b5fc21b3
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-09-06 16:52:13 +0200

    Add two small tests

--- tests/unit/models/QuickTranslationServiceTest.php
+++ tests/unit/models/QuickTranslationServiceTest.php
@@ -9,17 +9,23 @@ class QuickTranslationServiceTest extends TestBaseClass
     public static function setUpBeforeClass(): void
     {
         parent::setUpBeforeClass();
-        error_reporting(E_ALL);
 
         $surveyFile = self::$surveysFolder . '/limesurvey_survey_161359_quickTranslation.lss';
         self::importSurvey($surveyFile);
     }
 
-    public function testGetTranslations()
+    public function testGetTranslationsNoTranslation()
     {
-        var_dump(self::$surveyId);
         $qt = new QuickTranslation(self::$testSurvey);
-        $ts = $qt->getTranslations('title');
-        var_dump($ts);
+        $ts = $qt->getTranslations('title', 'aa');
+        $this->assertEmpty($ts);
+    }
+
+    public function testGetTranslationsEnglishTitle()
+    {
+        $qt = new QuickTranslation(self::$testSurvey);
+        $ts = $qt->getTranslations('title', 'en');
+        $this->assertNotEmpty($ts);
+        $this->assertEquals($ts[0]->surveyls_title, 'translation');
     }
 }
